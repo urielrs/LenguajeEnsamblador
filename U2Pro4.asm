9c8648807786f3e73b49a870d8262ef0267423e5
Uriel Rodríguez <162546336+urielrs@users.noreply.github.com>
2024-10-22 23:29:14 -0600

    Add files via upload

---
+++ U2Pro4.asm
@@ -0,0 +1,38 @@
+.model small;tama?o de memoria
+.stack;modelo de pila
+.data;inicio de datos
+x db 10
+y db 5
+mensaje db 10,13,7,"Numero menor", "$"
+mensaje2 db 10,13,7,"Numero mayor", "$"
+.code;inicio de codigo
+main proc;inicia proceso principal
+
+mov al,x
+mov bl,y
+
+cmp al,bl
+jmp mayor
+mayor:
+mov ax, SEG @data
+mov ds, ax
+
+mov ah,09h
+lea dx,mensaje2
+int 21h
+jmp fin
+menor:
+mov ax, SEG @data
+mov ds,ax
+
+mov ah,09h
+lea dx,mensaje
+int 21h
+
+jmp fin
+fin:
+mov ax,4c00h
+int 21h
+main endp
+
+end main
