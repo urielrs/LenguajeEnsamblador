fd603f48c8462c4595e7ccaabefd518316ac6a9d
Uriel Rodríguez <162546336+urielrs@users.noreply.github.com>
2024-11-03 22:57:16 -0600

    Add files via upload

---
+++ U3Pro5.asm
@@ -0,0 +1,43 @@
+.model small
+.stack 64
+.data
+msg db "mensaje$"
+.code
+inicio:
+mov ax,@data
+mov ds,ax
+ mov ah,09h
+ mov dx,offset msg
+ int 21h
+ mov ah,02
+ mov dl,0ah
+ int 21h
+ 
+ mov ah,02
+ mov dl,0dh
+ int 21h
+ 
+ mov SI,0
+ciclo:
+ mov dl,msg[SI]
+ cmp DL,24h
+ je fuera_ciclo
+ 
+ mov ah,02
+ mov dl,dl
+ int 21h
+ 
+ mov ah,02
+ mov dl,0ah
+ int 21h
+ 
+ inc SI
+ jmp ciclo
+fuera_ciclo:
+ 
+ mov ax,4c00h
+int 21h
+
+
+
+end inicio
